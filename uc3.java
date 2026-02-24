900e5a4b82e6dde263a4230137ea6e9014bb6a5a
Kartikeya <kr7990@srmist.edu.in>
2026-02-24 10:53:04 +0530

    this is main

---
+++ uc3.java
@@ -0,0 +1,25 @@
+public class Oops {
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****    ***** ");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *        *     *");
+        System.out.println(" *****   *****  *         ***** ");
+
+    }
+}
+class uc3 {
+    public static void main(String args[]) {
+
+        System.out.println(String.join("   ", "  *** "," ", "  *** "," ", "  **** "," ", " **** "));
+        System.out.println(String.join("   ", "**  **"," ", "**  **"," ", "**  **"," ", "**    "));
+        System.out.println(String.join("   ", "**  **"," ", "**  **"," ", "****  "," ", "**    "));
+        System.out.println(String.join("   ", "**  **"," ", "**  **"," ", "**    "," ", " ***  "));
+        System.out.println(String.join("   ", "**  **"," ", "**  **"," ", "**    "," ", "    **"));
+        System.out.println(String.join("   ", "**  **"," ", "**  **"," ", "**    "," ", "    **"));
+        System.out.println(String.join("   ", " *** "," ", "  *** "," ", " **    "," ", " **** "));
+    }
+}
